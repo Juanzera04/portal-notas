202920e77b3f9859b8552787433de134251a84ea
Juanzera04 <juanrodriguessilv4@gmail.com>
2025-11-14 13:41:35 -0300

    Update app.py

--- meu projeto/backend/app.py
+++ meu projeto/backend/app.py
@@ -1,1 +1,32 @@
-print("Olá Mundo")
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # permite acesso do frontend
+
+notas = {
+    "nicole": "",
+    "marcelo": "",
+    "juan": ""
+}
+
+@app.get("/notas")
+def get_notas():
+    return jsonify(notas)
+
+@app.post("/notas")
+def post_notas():
+    data = request.json
+    usuario = data.get("usuario")
+    texto = data.get("texto")
+    if usuario in notas:
+        notas[usuario] = texto
+    return jsonify(notas)
+
+@app.get("/")
+def home():
+    return "Backend funcionando!"
+
+# Apenas local, mas o Render ignora isso (bom!)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
